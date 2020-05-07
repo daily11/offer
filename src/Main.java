1b80a7f3be624a5aec199865acc7a7ab2c9cff18
ChenYixing <1663268062@qq.com>
2020-05-07 15:07:17 +0800

    update;

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
+import javacore.beanutils.Student;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        Object target = new Student();
+        Student source = new Student();
+        source.setName("cyx");
+        source.setAge(29);
+
+
     }
 }
