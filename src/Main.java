8006991937274c9c2503e7d460b34158616872bb
cyx <chenyixing@ubinavi.com.cn>
2020-06-13 23:08:57 +0800

    添加代码格式规约

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,6 @@
-import javacore.beanutils.Student;
-
-import java.beans.BeanInfo;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        Object target = new Student();
-        Student source = new Student();
-        source.setName("cyx");
-        source.setAge(29);
-
 
     }
+
 }
---
+++ src/代码格式.java
@@ -0,0 +1,22 @@
+public class 代码格式 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 2, 8, 1};
+        sort(arr);
+    }
+
+    public static void sort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " , ");
+        }
+    }
+}
